f853d5ed5d6cb2bda192570193e0ec8c1d04da79
Eliezer Encarnacion <eliezerencarnacion@gmail.com>
2014-02-08 20:35:37 -0500

    Added tracking for text. Will switch layout to Bootstrap

--- email_generator.js
+++ email_generator.js
@@ -6,17 +6,27 @@ $(document).ready(function() {
 	var first_click = true;
 
 	var maxSubjectAndPreviewLength = 130; //I calculated this number after testing the layout
+
+	// Append_Emails
 	for (var i = 0; i < Email.all.length; i++) {
 		var email = Email.all[i];
 		var subject = email.subject.substring(0,maxSubjectAndPreviewLength);
 		var previewSize = (subject.length + 1 >= maxSubjectAndPreviewLength) ? 0: maxSubjectAndPreviewLength - subject.length - 2;
 		var preview = email.content.substring(0, previewSize);
 
-		$('#email_list').append('<div class="email" id="email'+i+'"></div>');
+		$('#email_list').append('<div class="email tracked click" id="email'+i+'"></div>');
 		$('#email'+i).append('<span class="sender">' + email.sender.name + '</span>  ' + '<span class="subject">' + subject + '</span>\
 							  <span class="preview"> &nbsp;&ndash;&nbsp;' + preview + '</span>' + '<span class="date">' + email.formattedDate() + '</span>');
 	}
 
+	/* This function will take care of recording the log of events. It could do so by writing to a file,
+	outputting to the console, or any other way that seems reasonable */
+
+	var log_message = function(message) {
+		console.log(message);
+	}
+
+	//Open email
 	$(document).on('click', '.email', function() {
 		var id = $(this).attr('id');
 		var index = parseInt(id.substring(5)); //i.e, 'email5' will yield 5
@@ -33,13 +43,15 @@ $(document).ready(function() {
 
 	});
 
+	//Open New Email window
 	$('#new_email').click(function() {
 		$('#new_email_editor').show();	
 		$('#email_expanded').hide();
 		$('#email_list').hide();
 	});
 
-	$('#inbox').click(function() {
+	//Return to the main inbox
+	$('#nav_inbox').click(function() {
 		$('#email_expanded').empty();
 		
 		//Clear email editor of data
@@ -57,27 +69,29 @@ $(document).ready(function() {
 		$('#email_list').show();
 	});
 
-	var time;
-	$('#new_email_editor').find('input, #message_field, #predictions').on('mouseenter', function() {
-
-		time = new Date().getTime() / 1000;
+	/* Tracking functions */
+	var stamp;
+	$(document).on('mouseenter', '.tracked', function() {
+		stamp = new Date().getTime()/1000;
+		log_message('Entered element ' + $(this).prop('id')+'; timestamp: ' + stamp);
 	});
 
-	$('#new_email_editor').find('input, #message_field, #predictions').on('mouseleave', function() {
-
-		time = (new Date().getTime() / 1000) - time;
-		console.log('Hovered over ' +$(this).prop('id') + ' for ' + time * 1000 + 'milliseconds');
+	$(document).on('mouseleave', '.tracked', function() {
+		stamp = new Date().getTime()/1000;
+		log_message('Left element ' + $(this).prop('id')+'; timestamp: ' + stamp);
 	});
 
-	$('#new_email_editor').find('button').on('click', function() {
-		time = new Date().getTime()/1000;
-		console.log('Clicked ' + $(this).prop('id') +' at time ' + time);
+	$(document).on('click', '.click', function() {
+		stamp = new Date().getTime()/1000;
+		log_message('Clicked element ' + $(this).prop('id')+'; timestamp: ' + stamp);
 	});
 
-	$(document).on('click','.contact_suggestion', function() {
-		time = new Date().getTime()/1000;
-		console.log('Clicked ' + $(this).text() +' at time ' + time);
-	});
+	$(document).on('change keyup paste','.text', function() {
+		stamp = new Date().getTime()/1000;
+		log_message('Changed content of ' + $(this).prop('id') + ' to ' +
+					$(this).val() + ';timestamp: ' + stamp);
+	}); 
+
 
 	
 	//Make random suggestions
@@ -92,17 +106,18 @@ $(document).ready(function() {
 
 				var index = Math.floor(Math.random()*Contact.all.length);
 				var c = Contact.all[index];
-				$('#predictions').append('<a href="#" class="contact_suggestion" data-email="'+c.email+'">'+c.name+'</a>&nbsp');			
+				$('#predictions').append('<a href="#" class="contact_suggestion tracked click" id="'+c.email+'">'+c.name+'</a>&nbsp');
+				log_message('Contact ' + JSON.stringify(c) + ' was suggested');			
 			}
 			$('#predictions').show();
 		}
 	});
 
-	
 	$(document).on('click','.contact_suggestion', function() {
-		var email = $(this).data('email');
+		var email = $(this).prop('id');
 		var comma = (first_click) ? ' ': ', ';
 		$('#to_field').val($('#to_field').val() + comma + email); //append email to contents of to_field
+		log_message(email);
 		first_click = false;
 	});
 
