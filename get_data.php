6856a5b940a7f02669b2abddd17525e245c7adf0
Eliezer Encarnacion <eliezerencarnacion@gmail.com>
2014-02-14 15:46:10 -0500

    Fixed minor error

--- get_data.php
+++ get_data.php
@@ -1,7 +1,11 @@
 <?php
 $log_url = "/afs/cs.unc.edu/home/bartel/emailUI_tracking/";
 $data = $_POST["data"];
-$log_file = $_POST["file_name"];
-
-file_put_contents($log_url.$log_file."txt", $data);
+$log_name = $_POST["file_name"];
+$log_file = $log_url.$log_name."txt";
+$result = file_put_contents($log_file, $data);
+if ($result === FALSE) {
+	echo "Could not write\n";
+}
+echo $log_file;
 ?>
