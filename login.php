5aa707961cd73b34dc3bb45e77c4f0b91fe33ac5
Eliezer Encarnacion <eliezerencarnacion@gmail.com>
2014-02-18 11:32:40 -0500

    changed redirect on login.php from test 'google.com' to the actual EmailUI url

--- login.php
+++ login.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 $_SESSION["login"]="Yes, I'm in";
-header("Location: http://www.google.com");
-//header("Location: http://wwwx.cs.unc.edu/~bartel/cgi-bin/emailUI/EmailUI/");
+header("Location: http://wwwx.cs.unc.edu/~bartel/cgi-bin/emailUI/EmailUI/");
 die();
 ?>
 
