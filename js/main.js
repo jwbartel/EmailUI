f71f4a6aa87b68d9d4d169bb98b2d26e9f40f8cd
Eliezer Encarnacion <eliezerencarnacion@gmail.com>
2014-02-21 15:24:58 -0500

    Changed folder structure of project

---
+++ js/main.js
@@ -0,0 +1,165 @@
+$(document).ready(function() {
+
+
+	Email.generate();
+	var session_log;
+
+	//Do an ajax call to get log from an existing session
+		$.ajax({
+			type: 'GET',
+			url:"php/save_session_log.php",
+			success: function(data, status, jqXHR) {
+				session_log = data; //data should be a long string
+			}
+		});
+
+	var session_start = new Date();
+	log_message('Resumed session on: ' + session_start);
+
+	var already_suggested = false;
+	var first_click = true;
+	var email_editor_open = false;
+	var maxSubjectAndPreviewLength = 150; //I calculated this number after testing the layout
+
+
+	// Append_Emails
+	for (var i = 0; i < Email.all.length; i++) {
+		var email = Email.all[i];
+		var subject = email.subject.substring(0,maxSubjectAndPreviewLength);
+		var previewSize = (subject.length + 1 >= maxSubjectAndPreviewLength) ? 0: maxSubjectAndPreviewLength - subject.length - 2;
+		var preview = email.content.substring(0, previewSize);
+
+		$('ul.list-group').append('<li class="list-group-item email tracked click" id="email'+i+'"></li>');
+		$('#email'+i).append('<span class="sender">' + email.sender.name + '</span>  ' + '<span class="subject">' + subject + '</span>\
+							  <span class="preview"> &nbsp;&ndash;&nbsp;' + preview + '</span>' + '<span class="date">' + email.formattedDate() + '</span>');
+	}
+
+	/* This function will take care of recording the log of events. It could do so by writing to a file,
+	outputting to the console, or any other way that seems reasonable */
+	var log_message = function(message) {
+		console.log(message);
+		session_log += (message + '\n');
+	}
+
+	/* Returns a timestamp of the exact second since the sessions started
+	   For example, if session started at 3:00, this function will return
+	   60 at 3:01 */
+	var get_timestamp = function() {
+		return ((new Date().getTime()) - session_start.getTime())/1000;
+	}
+	
+	//Expand email
+	$(document).on('click', '.email', function() {
+
+
+		if (email_editor_open)
+			$('#email_expanded_col').attr('class','col-md-5')
+		else
+			$('#email_expanded_col').attr('class','col-md-10')
+
+		var id = $(this).attr('id');
+		var index = parseInt(id.substring(5)); //i.e, 'email5' will yield 5
+		var email = Email.all[index];
+
+		$('.panel-title').text(email.subject);
+		$('#email_expanded > .panel-body').append('<strong class="tracked" id="expanded_sender">' + email.sender.name +'</strong>' + '&lt;' + email.sender.email +'&gt<hr>');
+		$('#email_expanded > .panel-body').append('<p class="tracked" id="expanded_content">' + email.content +'</p>');
+		$('#email_expanded_col').show();
+		$('#email_list').hide();
+	});
+
+	//Open New Message editor
+	$('#nav_new_message').click(function() {
+		email_editor_open = true;
+
+		$('.preview').hide();
+		$('#email_expanded_col').attr('class', 'col-md-5');
+		$('#email_list').attr('class','col-md-5');
+		$('#new_message_editor').show();
+	});
+
+	//Return to the main inbox
+	$('#nav_inbox').click(function() {
+		$('#email_expanded > .panel-heading > .panel-title').empty();
+		$('#email_expanded > .panel-body').empty();
+		$('#email_expanded_col').hide();
+
+		if (email_editor_open)
+			$('#email_list').attr('class','col-md-5')
+		else
+			$('#email_list').attr('class','col-md-10')
+
+		$('#email_list').show();
+	});
+
+	/* Tracking functions */
+	$(document).on('mouseenter', '.tracked', function() {
+		log_message('Entered element ' + $(this).prop('id')+'; timestamp: ' + get_timestamp());
+	});
+
+	$(document).on('mouseleave', '.tracked', function() {
+		log_message('Left element ' + $(this).prop('id')+'; timestamp: ' + get_timestamp());
+	});
+
+	$(document).on('click', '.click', function() {
+		log_message('Clicked element ' + $(this).prop('id')+'; timestamp: ' + get_timestamp());
+	});
+
+	$(document).on('change keyup paste','.text', function() {
+		log_message('Changed content of ' + $(this).prop('id') + ' to ' +
+					$(this).val() + ';timestamp: ' + get_timestamp());
+	}); 
+
+	//Make random suggestions
+	$('#to_field').on('change keyup paste',function() {
+		var content = $(this).val();
+
+		if (content.indexOf(",") != -1 && !already_suggested) { //if there's a comma on the field
+			already_suggested = true;
+
+			//Select 3 random contacts and attach each
+			for (var i = 0; i < 3; i++) {
+
+				var index = Math.floor(Math.random()*Contact.all.length);
+				var c = Contact.all[index];
+				$('#predictions').append('<a href="#" class="contact_suggestion tracked click" id="'+c.email+'">'+c.name+'</a>&nbsp');
+				log_message('Contact ' + JSON.stringify(c) + ' was suggested');			
+			}
+			$('#predictions').show();
+		}
+	});
+
+	//Clicked Suggestion
+	$(document).on('click','.contact_suggestion', function() {
+		var email = $(this).prop('id');
+		var comma = (first_click) ? ' ': ', ';
+		$('#to_field').val($('#to_field').val() + comma + email); //append email to contents of to_field
+		log_message('Changed content of to_field to ' + $('#to_field').val() + ';timestamp: ' + get_timestamp());
+		first_click = false;
+	});
+
+	//End Session
+	$('#save_session_log').on('click', function() {
+        log_message('Session ended on: ' + new Date()+'\n');
+		$.ajax({
+			type: 'POST',
+			url:"php/save_session_log.php",
+			data: {'data':session_log, 'end_session': true},
+			success: function(data, status, jqXHR) {
+				window.location.replace(data); //load a thank you page
+			}
+		});
+	});
+
+	//Save session if window is closed
+	$(window).unload(function() {
+		$('#save_session_log').on('click', function() {
+        log_message('Session paused on: ' + new Date()+'\n');
+		$.ajax({
+			type: 'POST',
+			url:"php/save_session_log.php",
+			data: {'data':session_log, 'end_session':false},
+		});
+	});
+	});
+});
---
+++ php/index.php
@@ -0,0 +1,91 @@
+<?php 
+session_start();
+if (!isset($_SESSION["login"])) {
+    header("Location: http://wwwx.cs.unc.edu/~bartel/cgi-bin/emailUI/EmailUI/html/login.html");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> Sample Email </title>
+
+	<!-- Bootstrap -->
+    <link href="libs/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!--Custom CSS-->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+<div id="container">
+
+	<div class = "row" id="header">
+		<div class="col-md-12">
+			<h2> Email Prediction Test </h2>
+		</div>
+	</div>
+    <div class = "row" id="content">
+
+    	<div class="col-md-1" id="navigation">
+			<div class="btn-group-vertical" id="navigation">
+				<button type="button" class="btn btn-default tracked click" id="nav_new_message">New Message</button>
+				<button type="button" class="btn btn-default tracked click" id="nav_inbox">Inbox</button>
+  				<button type="button" class="btn btn-default tracked click" id="nav_important">Important</button>
+ 				<button type="button" class="btn btn-default tracked click" id="nave_sent">Sent</button>
+ 				<button type="button" class="btn btn-default tracked click" id="nav_drafts">Drafts</button>
+  				<button type="button" class="btn btn-default tracked click" id="nav_spam">Spam</button>
+ 				<button type="button" class="btn btn-default tracked click" id="nav_trash">Trash</button>
+                <button type="button" class="btn btn-danger tracked click" id="end_session">End Session</button>
+			</div>
+            
+		</div>
+
+
+        <div class ="col-md-10" id="email_list">
+        	<div class="panel panel-default">
+        		<ul class="list-group"></ul>
+        	</div>
+        </div>
+
+        <div class ="col-md-10" id="email_expanded_col">
+        	<div class="panel panel-default" id="email_expanded">
+  				<div class="panel-heading"><h3 class="panel-title tracked" id="expanded_subject"></h3></div>
+  				<div class="panel-body">
+  				</div>
+			</div>
+        </div>
+
+        <div class="col-md-5">
+        <div class ="panel panel-default"  id="new_message_editor">
+        	<div class="panel-heading">New Message</div>
+        	<div class="panel-body">
+            	<input type="text" class="form-control tracked text" id="to_field" autofocus="autofocus" placeholder="To"><br>
+            	<div id="predictions">
+                	<span> Consider including: </span>
+            	</div>
+            	<input type="text" class="form-control" id="cc_field" placeholder="Cc"><br>
+            	<input type="text" class="form-control tracked text" id="subect_field" placeholder="Subject">
+            	<br>
+            	<textarea rows="12" class ="form-control tracked text" id="message_field" placeholder="Write your message here"></textarea>
+            	<div id="new_message_options">
+                	<button type="button" class="btn btn-success tracked click" id="send"> Send </button>
+                	<button type="button" class="btn btn-warning tracked click" id="save"> Save Draft</button>
+                	<button type="button" class="btn btn-danger tracked click" id="discard"> Discard </button>
+           	 	</div>
+           	</div>
+		</div>
+		</div>
+	</div>
+    <!-- JS Files -->
+    <script src="libs/jquery.min.js"></script>
+    <script src="libs/Bootstrap/js/bootstrap.min.js"></script>
+
+	<script src="js/email.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+</html>
