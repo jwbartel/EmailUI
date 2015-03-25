8a64ab1a6a88164803c0ef8a8eab48036f89fa50
cphamlet <cphamlet@live.unc.edu>
2015-03-25 16:11:00 -0400

    Added color coding to inbox and outbox, Advance 1 day button
    Relates to inbox and outbox prediction times.

--- php/log.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* This is just for debugging purposes, to see what's in the current session */
-session_start();
-echo json_encode($_SESSION);
-?>
--- php/write_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$file = "../config.properties";
-
-$output = exec('/usr/bin/java -jar ConfigFileDriver.jar write '.$file);
-print $output;
-?>
